578579565f37284828012953fb4d7fde40243f26
ronaldochoiga <choigaronaldo@gmail.com>
2023-11-16 17:23:06 +0300

    the puppet and web_stack debugging

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,3 +1,3 @@
 # This is a puppet script that allows to make changes on login to holberton
 
-exec {'/usr/bin/env sed -i "s/holberton/foo" /etc/security/limits.config': }
+exec {'/usr/bin/env sed -i "s/holberton/foo/" /etc/security/limits.config': }
